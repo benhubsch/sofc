9f25cf003d68efc5989c3792bdcb5f3afd841e14
Ben Hubsch <bah37@duke.edu>
2018-05-11 21:53:07 -0700

    Basic sheet rendering.

---
+++ frontend/components/BasicSheet.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDataSheet from 'react-datasheet';
+import 'react-datasheet/lib/react-datasheet.css';
+
+export default class BasicSheet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [
+        [
+          { readOnly: true, value: '' },
+          { value: 'A', readOnly: true },
+          { value: 'B', readOnly: true },
+          { value: 'C', readOnly: true },
+          { value: 'D', readOnly: true }
+        ],
+        [{ readOnly: true, value: 1 }, { value: 1 }, { value: 3 }, { value: 3 }, { value: 3 }],
+        [{ readOnly: true, value: 2 }, { value: 2 }, { value: 4 }, { value: 4 }, { value: 4 }],
+        [{ readOnly: true, value: 3 }, { value: 1 }, { value: 3 }, { value: 3 }, { value: 3 }],
+        [{ readOnly: true, value: 4 }, { value: 2 }, { value: 4 }, { value: 4 }, { value: 4 }]
+      ]
+    };
+  }
+  render() {
+    return (
+      <ReactDataSheet
+        data={this.state.grid}
+        valueRenderer={(cell) => cell.value}
+        onContextMenu={(e, cell, i, j) => cell.readOnly ? e.preventDefault() : null}
+        // onCellsChanged={changes => {
+        //   const grid = this.state.grid.map(row => [...row]);
+        //   changes.forEach(({ cell, row, col, value }) => {
+        //     grid[row][col] = { ...grid[row][col], value };
+        //   });
+        //   this.setState({ grid });
+        // }}
+      />
+    );
+  }
+}
--- frontend/containers/AppContainer.js
+++ frontend/containers/AppContainer.js
@@ -2,11 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import Title from '../components/Title';
+import BasicSheet from '../components/BasicSheet';
 
 const AppContainer = ({ name }) => {
   return (
     <div>
-      <Title name={name} />
+      <Title name={ name } />
+      <BasicSheet />
     </div>
   );
 };
