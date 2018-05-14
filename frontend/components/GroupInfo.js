52c93ccf2faca90ed1f15b604d8f367b8ffdbff0
Ben Hubsch <bah37@duke.edu>
2018-05-13 19:08:56 -0700

    Working GroupInfo Component.

--- frontend/components/GroupInfo.js
+++ frontend/components/GroupInfo.js
@@ -4,10 +4,10 @@ import { Suggest } from '@blueprintjs/select';
 
 const GROUPS = [{
   name: 'Duke Catholic Center',
-  fundCode: 1111111
+  fundcode: 1111111
 }, {
   name: 'The Wild Ones',
-  fundCode: 2222222
+  fundcode: 2222222
 }];
 const SEARCH = 'Search for your group...';
 
@@ -18,6 +18,7 @@ export default class GroupInfo extends Component {
     this.handleItemSelect = this.handleItemSelect.bind(this);
     this.renderItem = this.renderItem.bind(this);
     this.renderInputValue = this.renderInputValue.bind(this);
+    this.filterItem = this.filterItem.bind(this);
     this.state = {
       group: GROUPS[0]
     };
@@ -39,48 +40,57 @@ export default class GroupInfo extends Component {
     return (
         <MenuItem
             active={ modifiers.active }
-            label={ group.fundCode.toString() }
-            key={ group.fundCode }
+            label={ group.fundcode.toString() }
+            key={ group.fundcode }
             onClick={ handleClick }
-            text={ text }
-            // text={ highlightText(text, query) }
+            // text={ text }
+            text={ this.highlightText(text, query) }
         />
     );
   }
 
-//   highlightText(text, query) {
-//     let lastIndex = 0;
-//     const words = query
-//         .split(/\s+/)
-//         .filter(word => word.length > 0)
-//         .map(escapeRegExpChars);
-//     if (words.length === 0) {
-//         return [text];
-//     }
-//     const regexp = new RegExp(words.join("|"), "gi");
-//     const tokens: React.ReactNode[] = [];
-//     while (true) {
-//         const match = regexp.exec(text);
-//         if (!match) {
-//             break;
-//         }
-//         const length = match[0].length;
-//         const before = text.slice(lastIndex, regexp.lastIndex - length);
-//         if (before.length > 0) {
-//             tokens.push(before);
-//         }
-//         lastIndex = regexp.lastIndex;
-//         tokens.push(<strong key={lastIndex}>{match[0]}</strong>);
-//     }
-//     const rest = text.slice(lastIndex);
-//     if (rest.length > 0) {
-//         tokens.push(rest);
-//     }
-//     return tokens;
-// }
+  highlightText(text, query) {
+    let lastIndex = 0;
+    const words = query
+        .split(/\s+/)
+        .filter(word => word.length > 0)
+        .map(this.escapeRegExpChars);
+    if (words.length === 0) {
+      return [text];
+    }
+    const regexp = new RegExp(words.join('|'), 'gi');
+    const tokens: React.ReactNode[] = [];
+    while (true) {
+      const match = regexp.exec(text);
+      if (!match) {
+        break;
+      }
+      const length = match[0].length;
+      const before = text.slice(lastIndex, regexp.lastIndex - length);
+      if (before.length > 0) {
+        tokens.push(before);
+      }
+      lastIndex = regexp.lastIndex;
+      tokens.push(<strong key={lastIndex}>{match[0]}</strong>);
+    }
+    const rest = text.slice(lastIndex);
+    if (rest.length > 0) {
+      tokens.push(rest);
+    }
+    return tokens;
+  }
+
+  escapeRegExpChars(text) {
+    return text.replace(RegExp(/([.*+?^=!:${}()|[]\/\\])/g), '\\$1');
+  }
 
   renderInputValue(group) {
     return group.name;
+  }
+
+  filterItem(query, group) {
+    const groupLower = `${group.name} ${group.fundcode.toString()}`.toLowerCase();
+    return groupLower.indexOf(query.toLowerCase()) >= 0;
   }
 
   render() {
@@ -89,9 +99,10 @@ export default class GroupInfo extends Component {
         items={ GROUPS }
         inputProps={{ placeholder: SEARCH }}
         itemRenderer={ this.renderItem }
+        itemPredicate={ this.filterItem }
         onItemSelect={ this.handleItemSelect }
         inputValueRenderer={ this.renderInputValue }
-        // noResults={<MenuItem disabled text="No results." />}
+        noResults={ <MenuItem disabled text="No results." /> }
       />
     );
   }
