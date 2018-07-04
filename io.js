50f9fea2b2eaad9359e80f2163350eee6ecbf38b
Ben Hubsch <bah37@duke.edu>
2018-07-03 22:26:41 -0700

    Restructure.

--- io.js
+++ io.js
@@ -3,7 +3,6 @@ const io = require('socket.io')();
 io.on('connection', socket => {
   console.log(`Socket connected: ${socket.id}`);
   socket.on('action', action => {
-    // do a write to db here
     socket.broadcast.emit('action', { ...action, ...{ isEmitted: true } });
   });
 });
