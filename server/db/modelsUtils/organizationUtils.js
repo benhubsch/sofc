d990d3af783fe84eab2fc2906924d3c866d8f89c
Ben Hubsch <bah37@duke.edu>
2018-07-05 21:10:58 -0700

    Catch-all state writes to db.

--- server/db/modelsUtils/organizationUtils.js
+++ server/db/modelsUtils/organizationUtils.js
@@ -5,10 +5,18 @@ const dbWrite = async (req, res) => {
   switch (req.body.action.type) {
     case types.ADD_CONTACTS:
     case types.REMOVE_CONTACTS:
-    case types.ADD_EMAILS: {
+    case types.REMOVE_EMAILS:
+    case types.ADD_EMAILS:
+    case types.SELECT_GROUP:
+    case types.FUNDCODE_CHANGE: {
       const organization = await models.Organization.findById(req.body.id);
       if (organization) {
-        await organization.update({ emails: req.body.emails });
+        await organization.update({
+          emails: req.body.emails,
+          contacts: req.body.contacts,
+          fundcode: req.body.fundcode,
+          name: req.body.name
+        });
         res.send({ id: organization.id });
       } else {
         const newOrganization = await models.Organization.create({
@@ -21,9 +29,6 @@ const dbWrite = async (req, res) => {
       }
       break;
     }
-    case types.REMOVE_EMAILS:
-    case types.SELECT_GROUP:
-    case types.FUNDCODE_CHANGE:
     default:
   }
 };
